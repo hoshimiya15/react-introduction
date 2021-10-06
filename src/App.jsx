22b563fe0ac85928142b6ae62a656b58e7cadf03
hoshimiya15 <91507674+hoshimiya15@users.noreply.github.com>
2021-10-06 13:21:52 +0900

    on/offで顔が消えたり現れたりする

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  //[stateの変数名, stateを更新していく関数名] = useState(変数の初期値)
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  //[stateの変数名, stateを更新していく関数名] = useState(変数の初期値)
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">おげんきですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^)</p>}
     </>
   );
 };
